79f9f231a9261685f1e827feeecb8644bd19806f
skflpoor <skflpoor@gmail.com>
2013-12-07 16:08:51 +0900

    build error frx

--- kjcodeTEST/ViewController.m
+++ kjcodeTEST/ViewController.m
@@ -19,11 +19,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
-    NSLog(@"TEST")
-    
-    
-    
-    fliquheroihfpiefhå
+    NSLog(@"TEST");
 }
 
 - (void)didReceiveMemoryWarning
