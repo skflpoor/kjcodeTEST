9c884102236dc2719accc78c6a3880dd97e6b50e
skflpoor <skflpoor@gmail.com>
2013-12-07 16:16:29 +0900

    Add ViewController

---
+++ kjcodeTEST/kjViewController.h
@@ -0,0 +1,13 @@
+//
+//  kjViewController.h
+//  kjcodeTEST
+//
+//  Created by 高 成洙 on 2013/12/07.
+//  Copyright (c) 2013年 Ko_Seong_soo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface kjViewController : UIViewController
+
+@end
