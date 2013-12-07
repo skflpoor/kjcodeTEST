5eb7611db41c2dfbb5a3f21d2eb062cd2a71dee4
skflpoor <skflpoor@gmail.com>
2013-12-07 15:29:20 +0900

    addTEST

---
+++ kjcodeTEST/testViewController.h
@@ -0,0 +1,13 @@
+//
+//  testViewController.h
+//  kjcodeTEST
+//
+//  Created by 高 成洙 on 2013/12/07.
+//  Copyright (c) 2013年 Ko_Seong_soo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface testViewController : UIViewController
+
+@end
---
+++ kjcodeTEST/testViewController.m
@@ -0,0 +1,38 @@
+//
+//  testViewController.m
+//  kjcodeTEST
+//
+//  Created by 高 成洙 on 2013/12/07.
+//  Copyright (c) 2013年 Ko_Seong_soo. All rights reserved.
+//
+
+#import "testViewController.h"
+
+@interface testViewController ()
+
+@end
+
+@implementation testViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
